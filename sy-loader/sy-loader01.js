953702e3afd40a3b2205de68bb3da27e8629b0e5
lizhiquan <842048365@qq.com>
2022-09-19 22:28:10 +0800

    同步、异步loader

--- sy-loader/sy-loader01.js
+++ sy-loader/sy-loader01.js
@@ -1,7 +1,18 @@
 // NormalLoader： 从后往前依次执行
+// 同步loader
 module.exports = function (content) {
   console.log("----这里是loader01----");
-  return content;
+  return content; // 1. 第一种返回方式
+  // this.calback(null, content) // 第二种返回方式
+};
+
+// 异步loader
+module.exports = function (content) {
+  console.log("----这里是loader01----");
+  const callback = this.async();
+  setTimeout(() => {
+    callback(null, content);
+  }, 1000);
 };
 
 // PitchLoader：从前往后依次执行
